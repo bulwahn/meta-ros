b83a15dd558b3a0d071ec1ff8623a4d715ea8ad4
Johannes Schrimpf <johannes.schrimpf@blueye.no>
2017-12-08 13:05:04 +0100

    scripts/check-versions.py: Add License notice

--- scripts/check-versions.py
+++ scripts/check-versions.py
@@ -4,6 +4,20 @@ __copyright__ = "Copyright 2017, Blueye Robotics AS"
 __credits__ = ["Johannes Schrimpf"]
 __license__ = "GPLv3"
 
+"""
+check-version.py is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+check-version.py is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+See <https://www.gnu.org/licenses/gpl-3.0.html> for full license text.
+"""
+
 import os
 import urllib.request
 import sys
